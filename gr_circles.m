9d257cf16c0a7533e3f4b7f372bafc0084eda834
Szymon Łukaszyk <65949607+szluk@users.noreply.github.com>
2020-12-09 20:06:44 +0100

    Update gr_circles.m

--- gr_circles.m
+++ gr_circles.m
@@ -1,37 +1,40 @@
 clear all
 % gr_circles.m
-% Calculate the number of vertices in subsequent circles centered 
-% at a vertex or at the centre of a ring of a hexagonal tessellation.
-% Radii rc of every vertex around a vertex are given by sqrt(A003136)
-% Radii rc of every vertex around a ring center are given by sqrt(A202822)
-% ----------------------------------------------------
-
+% Calculate the number of vertices of a hexagonal tessellation
+% that lie on subsequent circles centered 
+% at a vertex (OHV), or
+% at the center (OHC)
+% of one hexagon.
 % Plot the lattice and the circles.
+% Radii of every vertex around a vertex (OHV) are given by sqrt(A003136)
+% Radii of every vertex around the center (OHC) are given by sqrt(A202822)
 %
 % (c) Szymon Lukaszyk
 % email: szymon@patent.pl
 % licensed under MIT License
 % History
 % v1: 17.11.2020
-% v2: 08.12.2020 errors due to double vertices removed (thanks to Joerg Arndt)
+% v2: 08.12.2020 errors due to doubled vertices removed (thanks to Joerg Arndt for hinting them)
+% v3: 09.12.2020 improved roundoff errors handling
 % Currently this is just a script not a MATLAB function.
-% Due to roundoff errors it may (it will) give inaccurate results for large k.
+% Due to roundoff errors the script may (it will) give inaccurate results for large k.
 
-% FURTHER SERIOUS BUGS ARE PRESSUMED
+% FURTHER BUGS ARE ALSO PRESSUMED
+% ----------------------------------------------------
 
 % kind of circles
 center_at_vertex = 1;
-center_at_vertex = 0; % <=> center_at_ring_centre
+center_at_vertex = 0; % <=> center_at_hex_centre
 
 if center_at_vertex
-  disp 'ORIGIN AT HEX VERTEX'
+  OHwhatstr = 'ORIGIN AT HEX VERTEX'; % OHV
 else
-  disp 'ORIGIN AT HEX CENTER'
+  OHwhatstr = 'ORIGIN AT HEX CENTER'; % OHC
 end
+disp( OHwhatstr )
 
 % range of an initial unit square grid p with the origin 0,0
 % and size [-rng:1:rng]x[-rng:1:rng]
-rng = 100; 
 rng = 40; 
 
 % start with a unit square grid p with the origin 0,0
@@ -39,18 +42,17 @@ rng = 40;
 idx = 1;
 for i=-rng:rng % x
   for j=-rng:rng % y
-    %sprintf('%d,%d', i, j)
     p(idx,1) = i;
     p(idx,2) = j;    
     idx = idx+1;
   end
 end
-length(p) % 
+length(p)
 %(2*rng+1)^2
 %p
-disp 'unit square grid p created'
+disp 'unit square grid created'
 
-% convert unit square grid to A2 triangle grid np
+% convert unit square grid to A2 triangle grid
 % ----------------------------------------------------
 x_dlt = sqrt(3)/2;
 y_dlt = 3/2;
@@ -63,12 +65,12 @@ for k=1:length(p)
       idx = idx+1;
     end
 end
-length(np) % |np| = floor(length(p)/2) + 1
+length(np)
 %floor(length(p)/2) + 1
 %np
-disp 'A2 triangle grid np created'
+disp 'A2 triangle grid created'
 
-% create hehagon grid from A2 grid
+% create hehagonal grid from A2 grid
 % ----------------------------------------------------
 rngp_flg=1;
 idx = 1;
@@ -79,7 +81,6 @@ while k<=length(np)
       break
     end
     nnp(idx,:) = np(k+l,:);
-    %sprintf('%d,%d', k+l, idx)
     idx = idx+1;
   end
   if rngp_flg
@@ -92,9 +93,12 @@ while k<=length(np)
 end
 np = nnp;
 clear nnp
-disp 'hexagon grid np created'
+length(np)
+% #formula?
+%np
+disp 'hehagonal grid created'
 
-% rescale to a=1 and center
+% rescale the edge size to a=1 (and center the grid for OHC) 
 % ----------------------------------------------------
 for k=1:length(np)
   np(k,1) = np(k,1)/sqrt(3);
@@ -104,9 +108,9 @@ for k=1:length(np)
     np(k,2) = np(k,2);% + sqrt(3)/2;
   end 
 end
-disp 'hexagon grid np rescaled'
+disp 'hexagonal grid rescaled'
 
-% crop A2 triangle grid np into a square
+% crop hexagonal grid into a square
 % ----------------------------------------------------
 nplim = min(max(np))
 idx = 1;
@@ -114,18 +118,12 @@ for k=1:length(np)
   if ( abs( np(k,1) ) < nplim ) && ( abs( np(k,2) ) < nplim )
     nnp(idx,1) = np(k,1);
     nnp(idx,2) = np(k,2);   
-    %sprintf('%d,(%d,%d)', nnp(idx,1), nnp(idx,2))
     idx = idx+1;  
   end
 end
 np = nnp;
 clear nnp
-disp 'A2 triangle grid cropped'
-
-figure
-scatter(np(:,1), np(:,2),'.')
-hold on
-%return
+disp 'hexagonal grid cropped'
 
 hx=np;
 
@@ -135,6 +133,7 @@ for k=1:length(hx)
   rn(k) = ( hx(k,1)^2 + hx(k,2)^2 )^(1/2);
 end
 rn = sort(rn);
+[aa, bb, rn]= find( rn ); % get rid of zeros for the case of OHV
 %rn
 disp 'radii calculated'
 
@@ -149,20 +148,39 @@ for k=1:length(rn)
 end
 rn = rn1;
 clear rn1
-disp 'radii removed'
+disp 'out of the grid radii removed'
+
+% calculate differences between radii
+% ----------------------------------------------------
+for k=2:length(rn)
+  diff(k-1) = rn(k)-rn(k-1);
+end
+
+diff=sort(diff);
+for k=1:length(diff)
+  if diff(k) > 10^-13 % set with rng=300 %!! BUT THIS VALUE IS SURELY DERIVABLE FROM diff !!
+     kthr1 = k-1;  % max diff due to numerical errors
+     kthr2 = k;    % min diff due to graphene structure
+     break
+  end
+end
+disp 'differences calculated'
+
+diff(kthr1), diff(kthr2)
+%[aa, bb, diff]= find( diff ); % get rid of zeros
+%figure, plot( diff(1:length(diff)-5) ), title(OHwhatstr);
 
 % collect radii rnd having the same length
 % ----------------------------------------------------
-rnd = 0;
-idx = 1;
-rncnt = 0;
+rnd(1) = rn(1); % rn(1) = 1
+idx = 2;
+
+%rnd=0;
+%idx = 1;
 for k=1:length(rn)
-  rncnt = rncnt+1;
-  
   % take care of roundoff errors
-  %findres = find( abs( rnd-rn(k) ) < 10^(-13)  ); % 10^(-13) is OK up to rng=250, k=9999 (A003136 table)
-  %findres = find( abs( rnd-rn(k) ) < 10^(-12)  ); % 10^(-12) is OK to A202822 table
-  findres = find( abs( rnd-rn(k) ) < 10^(-13)  );
+  findres = find( abs( rnd-rn(k) ) < diff(kthr2) );
+  
   if isempty( findres )
     rnd(idx) = rn(k);
     idx = idx+1;         
@@ -173,17 +191,11 @@ disp 'same radii created'
 
 % count vertices at the same radius rn
 % ----------------------------------------------------
+
 idx = 1;
 count(idx) = 0;
-
-if center_at_vertex
-  kmin=2
-else
-  kmin=1
-end
-
-for k=kmin:length(rn)
-  if abs( rnd(idx)-rn(k) ) < 10^(-9)   
+for k=1:length(rn)
+  if abs( rnd(idx)-rn(k) ) < diff(kthr2)
     count(idx) = count(idx)+1;
   else
     idx = idx+1;
@@ -193,14 +205,43 @@ for k=kmin:length(rn)
     count(idx) = 1;
   end
 end
-
 count
+disp 'vertices counted'
 
-% validity check
+% gather unique count values
+% ----------------------------------------------------
+countun(1) = count(1);
+idx = 2;
+for k=2:length(count)
+  findres = find( countun == count(k) );
+  if isempty( findres )
+    countun(idx) = count(k);
+    idx = idx+1;         
+  end
+end
+countun=sort(countun);
+countun
+disp 'unique count values gathered'
+
+% validity check 1
+% ----------------------------------------------------
 sum(count) == length(rn)
+
+% validity check 2
+for k=1:length(count)
+  if mod( count(k), 3 ) ~= 0
+    disp( sprintf('Error for %d circle yielding %d count', k, count(k) ) )
+  end
+end   
+
+%return
 
 % draw circles around a vertex 0,0
 % ----------------------------------------------------
+figure
+scatter(hx(:,1), hx(:,2),'.')
+hold on
+
 colkind = 'r'; 
 %colkind = 'b'; 
 for k=1:length(rnd)
@@ -208,8 +249,6 @@ for k=1:length(rnd)
   xunit = rnd(k) * cos(th);
   yunit = rnd(k) * sin(th);
   plot(xunit, yunit, colkind);
-  %text( rnd(k), 0, sprintf('\\leftarrow %d', k) )      
-
   
   if colkind == 'r'
     colkind = 'b';
